64b64db9ce50cab44c44349bedbc5a745b50e548
Apurba123456 <126500102+Apurba123456@users.noreply.github.com>
2024-09-02 19:57:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   name     = "tfstate-resource-group"
-  location = "East US"
+  location = "Central India"
 }
 resource "azurerm_storage_account" "sa" {
   name                     = "tfstatestorageaccount"
@@ -8,10 +8,12 @@ resource "azurerm_storage_account" "sa" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  depends_on               = [azurerm_resource_group.rg] 
 }
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
+  depends_on            = [azurerm_resource_group.sa] 
 }
